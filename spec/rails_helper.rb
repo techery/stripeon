1d76c1282d9094029adfe63de209f4530b2326dd
Michael Kurtikov <michael.kurtikov@gmail.com>
2014-08-08 20:24:02 +0300

    Remove devise initializer.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,10 +26,6 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
-  config.include Warden::Test::Helpers, type: :feature
-
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
