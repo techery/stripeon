e0d3cc2b2dc7f258310d03ca67ce58bcffcda916
Michael Kurtikov <michael.kurtikov@gmail.com>
2014-08-11 12:39:21 +0300

    Move Stripe config to Stripeon initializer.

--- app/models/stripeon/configurator.rb
+++ app/models/stripeon/configurator.rb
@@ -2,5 +2,13 @@ module Stripeon
   class Configurator
     cattr_accessor :customer_model
     cattr_accessor :current_customer
+
+    def self.stripe_api_key=(api_key)
+      Stripe.api_key = api_key
+    end
+
+    def self.stripe_api_key
+      Stripe.api_key
+    end
   end
 end
